2bfe39f54f063ed5c04af270df6f8b4b162285af
Jonathan Powell <powell.jonj@gmail.com>
2024-05-03 16:52:57 -0400

    Finish initial pmd, spotbugs setup

---
+++ src/main/java/com/jjpowell/codequality/repository/InMemoryNotesRepository.java
@@ -0,0 +1,9 @@
+package com.jjpowell.codequality.repository;
+
+import java.util.Map;
+
+public class InMemoryNotesRepository {
+    Map<Integer, String> notes;
+
+    public InMemoryNotesRepository() {}
+}
